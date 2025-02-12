b398141dbf8e140ba51f3643e59de765c3441c70
Hüseyin Şimşek <huseyinsimsek7904@gmail.com>
2025-02-12 14:00:05 +0300

    feat: implement holding pieces and fix some issues

--- src/main.c
+++ src/main.c
@@ -24,9 +24,39 @@ jis-gui. If not, see <https://www.gnu.org/licenses/>.
 const int SQUARE_SIZE = 100;
 const Color GRID_WHITE_COLOR = (Color){0xf0, 0xf0, 0xf0, 0xff};
 const Color GRID_BLACK_COLOR = (Color){0xa0, 0xa0, 0xa0, 0xff};
+const Color GRID_HELD_COLOR = (Color){0xcc, 0xaa, 0x22, 0x80};
+
+const Vector2 BOARD_VECTOR = (Vector2){50, 50};
+
+// A position out of the bounds [0, 63] is an invalid position.
+static inline int to_position(int row, int col) { return (row << 3) + col; }
+
+static inline bool is_valid(int position) {
+  return position >= 0 && position < 64;
+}
+static inline int to_row(int position) { return position >> 3; }
+static inline int to_col(int position) { return position & 7; }
+
+#define POSITION_INV -1
+
+Vector2 pos_to_window_vec(int pos) {
+  return (Vector2){to_col(pos) * SQUARE_SIZE + BOARD_VECTOR.x,
+                   (7 - to_row(pos)) * SQUARE_SIZE + BOARD_VECTOR.y};
+}
+
+Rectangle pos_to_window_rect(int pos) {
+  return (Rectangle){to_col(pos) * SQUARE_SIZE + BOARD_VECTOR.x,
+                     (7 - to_row(pos)) * SQUARE_SIZE + BOARD_VECTOR.y,
+                     SQUARE_SIZE, SQUARE_SIZE};
+}
+
+int window_vec_to_id(Vector2 vec) {
+  return ((int)((vec.x - BOARD_VECTOR.x) / SQUARE_SIZE) +
+          (7 - (int)((vec.y - BOARD_VECTOR.y) / SQUARE_SIZE)) * 8);
+}
 
 int main(int argc, char *argv[]) {
-  InitWindow(800, 800, "JazzInSea - Cez GUI");
+  InitWindow(900, 900, "JazzInSea - Cez GUI");
 
   SetTargetFPS(60);
 
@@ -35,11 +65,13 @@ int main(int argc, char *argv[]) {
   {
     Color *pixel_data = malloc(64 * SQUARE_SIZE * SQUARE_SIZE * sizeof(Color));
 
+    // I do not believe that's the best way to do this.
+    // Rows are flipped since the topmost row is the 0th row.
     for (int row = 0; row < 8 * SQUARE_SIZE; row++) {
       for (int col = 0; col < 8 * SQUARE_SIZE; col++) {
         pixel_data[row * 8 * SQUARE_SIZE + col] =
-            (row / SQUARE_SIZE + col / SQUARE_SIZE) % 2 ? GRID_WHITE_COLOR
-                                                        : GRID_BLACK_COLOR;
+            (row / SQUARE_SIZE + col / SQUARE_SIZE) % 2 ? GRID_BLACK_COLOR
+                                                        : GRID_WHITE_COLOR;
       }
     }
 
@@ -60,44 +92,66 @@ int main(int argc, char *argv[]) {
   SetTextureFilter(black_pawn_texture, TEXTURE_FILTER_TRILINEAR);
   SetTextureFilter(black_knight_texture, TEXTURE_FILTER_TRILINEAR);
 
-  char board[8][8] = {
-      "np    PN", "pp    PP", "        ", "        ",
-      "        ", "        ", "PP    pp", "NP    pn",
-  };
+  char board[64] = {"np    PN"
+                    "pp    PP"
+                    "        "
+                    "        "
+                    "        "
+                    "        "
+                    "PP    pp"
+                    "NP    pn"};
+
+  int held_piece = POSITION_INV;
 
   while (!WindowShouldClose()) {
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      held_piece = window_vec_to_id(GetMousePosition());
+    } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+      held_piece = POSITION_INV;
+    }
+
     BeginDrawing();
     DrawTextureRec(grid_texture,
                    (Rectangle){0, 0, grid_texture.width, grid_texture.height},
-                   (Vector2){0, 0}, WHITE);
+                   BOARD_VECTOR, WHITE);
 
-    for (int row = 0; row < 8; row++) {
-      for (int col = 0; col < 8; col++) {
-        Texture2D *texture;
-        switch (board[row][col]) {
-        case 'P':
-          texture = &white_pawn_texture;
-          break;
-        case 'N':
-          texture = &white_knight_texture;
-          break;
-        case 'p':
-          texture = &black_pawn_texture;
-          break;
-        case 'n':
-          texture = &black_knight_texture;
-          break;
-        default:
-          continue;
-        }
+    if (held_piece >= 0) {
+      DrawRectangleRec(pos_to_window_rect(held_piece), GRID_HELD_COLOR);
+    }
 
-        // Upsize the texture to the square size and draw it.
-        DrawTexturePro(*texture,
-                       (Rectangle){0, 0, texture->width, texture->height},
-                       (Rectangle){col * SQUARE_SIZE, row * SQUARE_SIZE,
-                                   SQUARE_SIZE, SQUARE_SIZE},
-                       (Vector2){0, 0}, 0, WHITE);
+    // Rows are inverted, unlike how jazz-in-sea represents them.
+    for (int position = 0; position < 64; position++) {
+      Texture2D *texture;
+      switch (board[position]) {
+      case 'P':
+        texture = &white_pawn_texture;
+        break;
+      case 'N':
+        texture = &white_knight_texture;
+        break;
+      case 'p':
+        texture = &black_pawn_texture;
+        break;
+      case 'n':
+        texture = &black_knight_texture;
+        break;
+      default:
+        continue;
       }
+
+      Rectangle rect;
+      if (position == held_piece) {
+        int x = GetMouseX() - SQUARE_SIZE / 2;
+        int y = GetMouseY() - SQUARE_SIZE / 2;
+        rect = (Rectangle){x, y, SQUARE_SIZE, SQUARE_SIZE};
+      } else {
+        rect = pos_to_window_rect(position);
+      }
+
+      // Upsize the texture to the square size and draw it.
+      DrawTexturePro(*texture,
+                     (Rectangle){0, 0, texture->width, texture->height}, rect,
+                     (Vector2){0, 0}, 0, WHITE);
     }
 
     EndDrawing();
